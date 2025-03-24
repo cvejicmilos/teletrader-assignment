b9879a05b7a2a7d5cece600679a16e5f9978a26f
cvejicmilos <mlscvjc@gmail.com>
2025-03-24 20:57:16 +0100

    feature: Add functions for registering and authenticating users

--- teletrader/src/main/java/com/teletrader/teletrader/auth/AuthenticationController.java
+++ teletrader/src/main/java/com/teletrader/teletrader/auth/AuthenticationController.java
@@ -1,5 +1,7 @@
 package com.teletrader.teletrader.auth;
 
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,19 +10,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request
     ) {
-
+        return ResponseEntity.of(authenticationService.register(request));
     }
 
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> authenticate(
             @RequestBody AuthenticationRequest request
     ) {
-
+        return ResponseEntity.of(authenticationService.authenticate(request));
     }
 }
---
+++ teletrader/src/main/java/com/teletrader/teletrader/auth/AuthenticationService.java
@@ -0,0 +1,18 @@
+package com.teletrader.teletrader.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ProblemDetail;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    // TODO: Implement
+    public AuthenticationResponse register(RegisterRequest request) {
+        return null;
+    }
+    // TODO: Implement
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        return null;
+    }
+}
